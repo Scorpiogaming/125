a4c1e6d4a4447af1cefa1ba5d122bd7e12d8c459
Scorpiogaming <74415665+Scorpiogaming@users.noreply.github.com>
2021-07-28 13:27:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask,jsonify, request
+
+app = Flask(__name__)
+@app.route("/add-data", methods=["POST"])
+def predict_data():
+    image=request.files.get("alphabet")
+    prediction=get_prediction(image)
+    return jsonify({
+       'prediction': prediction
+    }),200
+    
+if (__name__ == "__main__"):
+    app.run(debug=True)
